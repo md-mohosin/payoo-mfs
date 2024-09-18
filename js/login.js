b4b528baf0acf53137bb57a7e41479b5818517dc
md-mohosin <mdmohosin.web@gmail.com>
2024-09-18 23:47:35 +0600

    login sectio add

---
+++ js/login.js
@@ -0,0 +1,5 @@
+console.log('connected in html file');
+document.getElementById('button-login').addEventListener('click', function(){
+    const inputField = document.getElementById('input-number')
+    console.log(inputField.value);
+})
